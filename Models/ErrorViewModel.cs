f6c6cb005c533b7cf28f97c6132ce9aef4d8dbe4
BOGDAN-ALEXANDRU VARGA <bogdan.alexandru.varga@stud.ubbcluj.ro>
2020-10-30 14:30:00 +0200

    !

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Varga_Bogdan_lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
